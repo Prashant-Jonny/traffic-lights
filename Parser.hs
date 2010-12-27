a4b946a0ae582be0740a79d920b0f40b9f60d51c
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-12-27 21:40:56 +0300

    parser works

--- Parser.hs
+++ Parser.hs
@@ -2,6 +2,7 @@ module Parser (parse) where
 
 import Types
 import Data.Char
+import Data.List
 import qualified Text.ParserCombinators.Parsec as P
 
 parse :: String -> Program
@@ -16,8 +17,52 @@ parse' input = case res of
 
 
 unlabel :: ProgramWithLabels -> Program
-unlabel = undefined
+unlabel prog = map (unlabel_command lps) $ zip prog [0..(length prog)]
+    where lps = labelPositions prog
 
+unlabel_command :: [(Label, Shift)] -> (LCommand, Shift) -> Command
+unlabel_command labelPositions lcommand =
+    case lcommand of
+      ((Store' x), _) -> Store x 
+      ((Inc'   r), _) -> Inc r   
+      ((Dec'   r), _) -> Dec r   
+      ((Not'   r), _) -> Not r   
+      ((And'   r), _) -> And r   
+      ((Or'    r), _) -> Or  r   
+      ((Xor'   r), _) -> Xor r   
+      ((Xch'   r), _) -> Xch r   
+      ((Jz'    s), _) -> Jz  s
+      ((Jnz'   s), _) -> Jnz s   
+      ((Jzl    l), s) -> Jz  (label2shift labelPositions s l)
+      ((Jnzl   l), s) -> Jnz (label2shift labelPositions s l)
+      ((Shr'   s), _) -> Shr s
+      ((Shl'   s), _) -> Shl s
+      ((Jmp'   r), _) -> Jmp r    
+      ((Out'   r), _) -> Out r    
+      ((Sleep' r), _) -> Sleep r  
+      (Nop',     _) -> Nop      
+      ((Labeled lcommand label), s) -> unlabel_command labelPositions (lcommand, s)
+
+
+labelPositions :: ProgramWithLabels -> [(Label, Shift)]
+labelPositions prog = map l . filter labels $ zip prog [0..(length prog)] 
+    where labels (command, _) = case command of
+                                  Labeled _ _ -> True
+                                  otherwise -> False
+          l ((Labeled c l), pos) = (l, pos)
+
+label2shift :: [(Label, Shift)] -> Shift -> Label -> Shift
+label2shift lps s label = case p of 
+                            Just smth -> encode((snd smth) - s)
+                            otherwise -> error "Label not found"
+    where pred (l, pos) = l == label
+          p = find pred lps
+
+encode :: Int -> Int
+encode x = if (x >= 0 && x < 8) then x
+           else 7 - x
+
+-- Parsing:
 
 dig :: P.GenParser Char st Int
 dig = do {d <- P.digit; return (ord d - ord '0')}
@@ -31,27 +76,28 @@ nat = do ds <- P.many1 dig
 register :: P.GenParser Char st Reg
 register = do {P.char 'R'; r <- nat; return (toEnum r)}
 
-spaces = P.many1 P.space
+spaces = P.many P.space
+spaces1 = P.many1 P.space
 
-store  = do {P.string "Store"; P.spaces; val <- nat;      spaces; return (Store' val)}
-inc    = do {P.string "Inc";   P.spaces; reg <- register; spaces; return (Inc' reg)}
-dec    = do {P.string "Dec";   P.spaces; reg <- register; spaces; return (Dec' reg)}
-_not   = do {P.string "Not";   P.spaces; reg <- register; spaces; return (Not' reg)}
-_and   = do {P.string "And";   P.spaces; reg <- register; spaces; return (And' reg)}
-_or    = do {P.string "Or";    P.spaces; reg <- register; spaces; return (Or' reg)}
-_xor   = do {P.string "Xor";   P.spaces; reg <- register; spaces; return (Xor' reg)}
-xch    = do {P.string "Xch";   P.spaces; reg <- register; spaces; return (Xch' reg)}
-jz     = do {P.string "Jz";    P.spaces; val <- nat;      spaces; return (Jz' val)}
-jnz    = do {P.string "Jnz";   P.spaces; val <- nat;      spaces; return (Jnz' val)}
-jzl    = do {P.string "Jz";    P.spaces; val <- P.many (P.noneOf " \n\r\t"); spaces; return (Jzl val)}
-jnzl   = do {P.string "Jnz";   P.spaces; val <- P.many (P.noneOf " \n\r\t"); spaces; return (Jnzl val)}
-shr    = do {P.string "Shr";   P.spaces; val <- nat;      spaces; return (Shr' val)}
-shl    = do {P.string "Shl";   P.spaces; val <- nat;      spaces; return (Shl' val)}
-jmp    = do {P.string "Jmp";   P.spaces; reg <- register; spaces; return (Jmp' reg)}
-out    = do {P.string "Out";   P.spaces; reg <- register; spaces; return (Out' reg)}
-_sleep = do {P.string "Sleep"; P.spaces; reg <- register; spaces; return (Sleep' reg)}
-nop    = do {P.string "Nop";   P.spaces; return Nop'}
-label  = do {l <- P.many (P.noneOf " \n\r\t:"); P.char ':'; P.spaces; c <- command; return (Labeled c l)}
+store  = do {P.string "Store"; spaces1; val <- nat;      spaces; return (Store' val)}
+inc    = do {P.string "Inc";   spaces1; reg <- register; spaces; return (Inc' reg)}
+dec    = do {P.string "Dec";   spaces1; reg <- register; spaces; return (Dec' reg)}
+_not   = do {P.string "Not";   spaces1; reg <- register; spaces; return (Not' reg)}
+_and   = do {P.string "And";   spaces1; reg <- register; spaces; return (And' reg)}
+_or    = do {P.string "Or";    spaces1; reg <- register; spaces; return (Or' reg)}
+_xor   = do {P.string "Xor";   spaces1; reg <- register; spaces; return (Xor' reg)}
+xch    = do {P.string "Xch";   spaces1; reg <- register; spaces; return (Xch' reg)}
+jz     = do {P.string "Jz";    spaces1; val <- nat;      spaces; return (Jz' val)}
+jnz    = do {P.string "Jnz";   spaces1; val <- nat;      spaces; return (Jnz' val)}
+jzl    = do {P.string "Jz";    spaces1; val <- P.many (P.noneOf " \n\r\t"); spaces; return (Jzl val)}
+jnzl   = do {P.string "Jnz";   spaces1; val <- P.many (P.noneOf " \n\r\t"); spaces; return (Jnzl val)}
+shr    = do {P.string "Shr";   spaces1; val <- nat;      spaces; return (Shr' val)}
+shl    = do {P.string "Shl";   spaces1; val <- nat;      spaces; return (Shl' val)}
+jmp    = do {P.string "Jmp";   spaces1; reg <- register; spaces; return (Jmp' reg)}
+out    = do {P.string "Out";   spaces1; reg <- register; spaces; return (Out' reg)}
+_sleep = do {P.string "Sleep"; spaces1; reg <- register; spaces; return (Sleep' reg)}
+nop    = do {P.string "Nop";   spaces1; return Nop'}
+label  = do {l <- P.many (P.noneOf " \n\r\t:"); P.char ':'; spaces1; c <- command; return (Labeled c l)}
 
 
 command :: P.GenParser Char st LCommand
