132ddd402c95fc27893f85bad2194e632e01096b
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-12-27 20:18:41 +0300

    Separate module for emulator

--- interpreter.hs
+++ interpreter.hs
@@ -6,72 +6,7 @@ import Data.Word
 import qualified Data.ByteString.Lazy as L
 import List
 import Types
-
-type Shift = Int
-
-type ProgramWithLabels = [LCommand]
-
-type Program    = [Command]
-type Registers  = [Int]
-type CommandCnt = Int
-type WmState    = (Registers, CommandCnt)
-
-initialState :: WmState
-initialState = ([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 0)
-
-run :: Program -> IO WmState
-run p = run' p initialState
-
-run' :: Program -> WmState -> IO WmState
-run' program (registers, counter) = runCommand command (registers, counter) >>= h
-    where command = program !! counter
-          h newState = if (halt newState) then return newState else run' program newState
-
-halt :: WmState -> Bool
-halt (registers, _) = getReg registers R0 == 100
-
-runCommand :: Command -> WmState -> IO WmState
-runCommand command (state, cnt) =
-    case command of 
-      Out   reg -> (putStrLn $ show (getReg state reg)) >> return (update command (state, cnt))
-      Sleep reg -> usleep ((getReg state reg) * 1000)   >> return (update command (state, cnt))
-      otherwise -> return (update command (state, cnt))
-
-update :: Command -> WmState -> WmState
-update command (state, cnt) = (updateRegisters command state, updateCounter command (state, cnt))
-
-updateRegisters :: Command -> Registers -> Registers
-updateRegisters command state =
-    case command of 
-      Store x   -> setReg state R0 x                                             
-      Inc reg   -> setReg state reg ((getReg state reg)+1)                       
-      Dec reg   -> setReg state reg ((getReg state reg)-1)                       
-      Not reg   -> setReg state reg (complement (getReg state reg))              
-      And reg   -> setReg state R0  ((getReg state R0) .&. (getReg state R0))    
-      Or  reg   -> setReg state R0  ((getReg state R0) .|. (getReg state R0))    
-      Xor reg   -> setReg state R0  ((getReg state R0) `xor` (getReg state R0))  
-      Xch reg   -> setReg (setReg state R0 regval) reg r0val
-        where r0val  = getReg state R0
-              regval = getReg state reg
-      otherwise -> state
-
-      
-updateCounter :: Command -> WmState -> CommandCnt
-updateCounter command (state, cnt) =
-    case command of 
-      Jz  shift -> if (getReg state R0) == 0 then cnt+decode(shift) else cnt+1     
-      Jnz shift -> if not $ (getReg state R0) == 0 then cnt+decode(shift) else cnt+1 
-      Shr shift -> cnt+decode(shift)                                              
-      Shl shift -> cnt+decode(shift)                                              
-      Jmp reg   -> getReg state reg                                               
-      otherwise -> cnt+1
-
-getReg :: Registers -> Reg -> Int
-getReg registers reg = registers !! fromEnum(reg)
-
-setReg :: Registers -> Reg -> Int -> Registers
-setReg registers reg val = take r registers ++ [val] ++ drop (r+1) registers
-    where r = fromEnum reg
+import Emulator
 
 compile :: [(Exc Command)] -> L.ByteString
 compile = L.pack . map compileCommand
@@ -99,11 +34,6 @@ compileCommand' command =
       Out reg   -> 0xd0 .|. fromIntegral(fromEnum(reg))  
       Sleep reg -> 0xe0 .|. fromIntegral(fromEnum(reg))  
       Nop       -> 0xff                                  
-
-
-decode :: Int -> Int
-decode x = if (x >= 0 && x < 8) then x
-           else -(x `mod` 8 + 1)
 
 encode :: Int -> Int
 encode x = if (x >= 0 && x < 8) then x
