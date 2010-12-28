022dc081732ce9df5778f522aa8bb3793141073f
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-12-28 18:07:02 +0300

    add decompile

--- Compiler.hs
+++ Compiler.hs
@@ -1,4 +1,4 @@
-module Compiler (compile) where
+module Compiler (compile, decompile) where
 
 import Types
 import qualified Data.ByteString.Lazy as L
@@ -27,3 +27,33 @@ compileCommand command =
       Out reg   -> 0xd0 .|. fromIntegral(fromEnum(reg))  
       Sleep reg -> 0xe0 .|. fromIntegral(fromEnum(reg))  
       Nop       -> 0xff                                  
+
+
+decompile :: L.ByteString -> Program
+decompile = map decompileCommand . L.unpack
+
+decompileCommand :: Word8 -> Command
+decompileCommand byte = decompileCommand' command arg
+    where command = shift byte (-4)
+          arg = byte .&. 0x0f
+                     
+
+decompileCommand' :: Word8 -> Word8 -> Command
+decompileCommand' command arg
+    | command == 0  = Store (fromIntegral arg)
+    | command == 1  = Inc   (toEnum (fromIntegral arg))
+    | command == 2  = Dec   (toEnum (fromIntegral arg))
+    | command == 3  = Not   (toEnum (fromIntegral arg))
+    | command == 4  = And   (toEnum (fromIntegral arg))
+    | command == 5  = Or    (toEnum (fromIntegral arg))
+    | command == 6  = Xor   (toEnum (fromIntegral arg))
+    | command == 7  = Xch   (toEnum (fromIntegral arg))
+    | command == 8  = Jz    (fromIntegral arg)
+    | command == 9  = Jnz   (fromIntegral arg)
+    | command == 10 = Shr   (fromIntegral arg)
+    | command == 11 = Shl   (fromIntegral arg)
+    | command == 12 = Jmp   (toEnum (fromIntegral arg))
+    | command == 13 = Out   (toEnum (fromIntegral arg))
+    | command == 14 = Sleep (toEnum (fromIntegral arg))
+    | command == 15 && arg == 15 = Nop
+
